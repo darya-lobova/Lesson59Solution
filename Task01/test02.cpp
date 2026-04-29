e352de4b2ad44fb44d90a777b54755cfe893551e
st <st@A510PC3>
2026-04-29 17:39:58 +0300

    Task1 update1

--- Task01/test02.cpp
+++ Task01/test02.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int size = 10;
-
-	int* array = new int[size];
-
-	for (int i = 0; i < size; i++)
-	{
-		*(array + i) = 0; //array[i]
-	}
-
-	for (int i = 0; i < size; i++)
-	{
-		cout << *(array + i) << "\t";  //array[i]
-	}
-
-	delete[] array;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int size = 10;
+//
+//	int* array = new int[size];
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		*(array + i) = 0; //array[i]
+//	}
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << *(array + i) << "\t";  //array[i]
+//	}
+//
+//	delete[] array;
+//
+//	return 0;
+//}
---
+++ Task1/util.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print(string s);
+void init_random(int* array, int size, int a, int b);
+void clear(int* array, int size);
+int* create(int size);
+void remove(int* array);
+string convert(int* array, int size);
